5121d315d1742038de9ce4f8901ad45eb21a5d2f
KaterinkaIzotova <147706130+KaterinkaIzotova@users.noreply.github.com>
2023-10-19 18:19:31 +0300

    Обновила
    +

--- Algorithm.java
+++ Algorithm.java
@@ -1,34 +1,50 @@
 import java.util.Scanner;
 public class Algorithm {
     int number;
-    public Algorithm (int number) {
+    public Algorithm(int number) {
         this.number = number;
     }
-    public int getNumber () {
+    public char[] getNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите число:");
         int t;
         String x;
         var l = Integer.toString(number);
-
-        while ((t = String.valueOf(x = scan.nextLine()).length()) > l.length() || t < l.length())   {
-            System.out.println("Неверное число. Повторите попытку:");
+        while ((t = String.valueOf(x = scan.nextLine()).length()) > l.length() || t < l.length()) {
+            if (x.equals("сдаюсь")) {
+                System.out.println("Игра окончена");
+                System.exit(0);
+            } else {
+                System.out.println("Неверное число. Повторите попытку:");
+            }
         }
         System.out.println("Введенное число:" + x);
-
         char[] myNumber = x.toCharArray();
-        char[] pkNumber = l.toCharArray();
+        return myNumber;
+    }
+    public int findNumber(char[] myNumber, int t) {
+        var k = Integer.toString(t);
+        char[] pkNumber = k.toCharArray();
         int bulls = 0;
-
-        String[] result = new String[myNumber.length];
+        int cows = 0;
         for (int i = 0; i < myNumber.length; i++) {
             if (myNumber[i] == pkNumber[i]) {
                 bulls++;
             }
-            result[i] = String.valueOf(i);
+            for (int j = 0; j < myNumber.length; j++) {
+                if (myNumber[i] == pkNumber[j]) {
+                    cows++;
+                }
+            }
         }
-        int cows = 0;
-
-        return t;
+        if (bulls == pkNumber.length) {
+            System.out.println("Количество быков:" + bulls);
+            System.out.println("Загаданное число: " + number);
+            System.out.println("Вы выиграли!");
+        } else {
+            System.out.println("Количество быков:" + bulls);
+            System.out.println("Количество коров:" + (cows - bulls));
+        }
+        return bulls;
     }
 }
--- Main.java
+++ Main.java
@@ -1,39 +1,61 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random rn = new Random();
 
-        System.out.println("Выберите сложность игры");
+        System.out.println("Выберите сложность игры: 3, 4, 5");
         int x = sc.nextInt();
-
-        // Не пон как сделать разные цифры в числе (типа исключить 2234 и подобные)
+        // Не понимаю как сделать разные цифры в числе (типа исключить 2234 и подобные)
         if (x == 3) {
             int a=100;
             int b=999;
+            int cnt = 0;
+            int cntBulls = 0;
             int randomNum1 = a + (int)(Math.random() * ((b - a) + 1));
-            System.out.println("Случайное число x: " + randomNum1);
+
+            // System.out.println("Случайное число x: " + randomNum1);
             Algorithm myAlgorithm = new Algorithm(randomNum1);
-            myAlgorithm.getNumber();
+            do {
+                cntBulls = myAlgorithm.findNumber(myAlgorithm.getNumber(),randomNum1);
+                cnt ++;
+            }
+            while (cntBulls != 3);
+            System.out.println("Количество попыток: " + cnt);
 
         }
         else if (x == 4) {
             int a = 1000;
             int b = 9999;
-            int randomNum2 = a + (int) (Math.random() * ((b - a) + 1));
-            System.out.println("Случайное число x: " + randomNum2);
+            int cnt = 0;
+            int cntBulls = 0;
+            int randomNum2 = a + (int)(Math.random() * ((b - a) + 1));
+
+            // System.out.println("Случайное число x: " + randomNum1);
             Algorithm myAlgorithm = new Algorithm(randomNum2);
-            myAlgorithm.getNumber();
+            do {
+                cntBulls = myAlgorithm.findNumber(myAlgorithm.getNumber(),randomNum2);
+                cnt ++;
+            }
+            while (cntBulls != 4);
+            System.out.println("Количество попыток: " + cnt);
+
         }
         else if (x == 5) {
             int a = 1000;
             int b = 99999;
-            int randomNum3 = a + (int) (Math.random() * ((b - a) + 1));
-            System.out.println("Случайное число x: " + randomNum3);
-            Algorithm myAlgorithm = new Algorithm(randomNum3);
-            myAlgorithm.getNumber();
+            int cnt = 0;
+            int cntBulls = 0;
+            int randomNum1 = a + (int)(Math.random() * ((b - a) + 1));
+
+            // System.out.println("Случайное число x: " + randomNum1);
+            Algorithm myAlgorithm = new Algorithm(randomNum1);
+            do {
+                cntBulls = myAlgorithm.findNumber(myAlgorithm.getNumber(),randomNum1);
+                cnt ++;
+            }
+            while (cntBulls != 5);
+            System.out.println("Количество попыток: " + cnt);
         }
         else {
             System.out.println("Вы ввели некорректное число. Конец игры.");
